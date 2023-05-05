f5a25674075485f3863c0b12cd730f33b1ccf1bc
Josh Hawk <hawkjosh@icloud.com>
2023-05-04 23:16:34 -0400

    update push 5/4/23 @ 11:16pm

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,30 @@ export const App = () => {
 	const [search, setSearch] = useState('')
 
 	const getSongData = async (search) => {
-		const requestUrl = `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=5/`
+		const requestUrl = `https://spotify23.p.rapidapi.com/search/?q=${search}&type=multi&offset=0&limit=10&numberOfTopResults=5`
 		const options = {
 			method: 'GET',
 			headers: {
-				'X-RapidAPI-Key': '7e218f9294mshed130077e09c28bp11d981jsnb67b9fe814b1',
-				'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
+				'X-RapidAPI-Key': 'd402ff8d8amshb632d3dff23fa99p160c6bjsn9fc2d552e490',
+				'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+			},
+		}
+		try {
+			const response = await fetch(requestUrl, options)
+			const result = await response.json()
+			console.log(result)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	const getVideoData = async (search) => {
+		const requestUrl = `https://youtube-v31.p.rapidapi.com/search?q=${search}&part=snippet%2Cid&regionCode=US&maxResults=50&order=date`
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': 'd402ff8d8amshb632d3dff23fa99p160c6bjsn9fc2d552e490',
+				'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
 			},
 		}
 		try {
@@ -27,114 +45,9 @@ export const App = () => {
 	const newSearch = (e) => {
 		e.preventDefault()
 		getSongData(search)
+		getVideoData(search)
+		setSearch('')
 	}
-
-	// request
-	// submitEl.addEventListener('click', () => {
-	// 	if (resultsEl.children[0]) {
-	// 		reset(
-	// 			resultsEl.children[0],
-	// 			resultsEl.children[1],
-	// 			resultsEl.children[2],
-	// 			resultsEl.children[3],
-	// 			resultsEl.children[4]
-	// 		)
-	// 	}
-	// 	const requestUrl =
-	// 		'https://shazam.p.rapidapi.com/search?term=' +
-	// 		searchEl.value +
-	// 		'&locale=en-US&offset=0&limit=5/'
-	// 	const options = {
-	// 		method: 'GET',
-	// 		headers: {
-	// 			'X-RapidAPI-Key': '7e218f9294mshed130077e09c28bp11d981jsnb67b9fe814b1',
-	// 			'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
-	// 		},
-	// 	}
-	// 	fetch(requestUrl, options)
-	// 		.then((response) => response.json())
-	// 		.then((response) => createResults(response))
-
-	// 		.catch((err) => console.error(err))
-	// })
-
-	// function createResults(response) {
-	// 	for (let i = 0; i < response.tracks.hits.length; i++) {
-	// 		// creates display elements
-	// 		let resultsTab = document.createElement('div')
-	// 		let resultsName = document.createElement('h3')
-	// 		let resultImg = document.createElement('img')
-	// 		let shazamEl = document.createElement('a')
-
-	// 		// sets track name and shazam message
-	// 		resultsName.innerHTML = response.tracks.hits[i].track.share.subject
-	// 		shazamEl.innerHTML = 'Listen on Shazam'
-
-	// 		// set class and attributes for style and links
-	// 		resultImg.setAttribute(
-	// 			'src',
-	// 			response.tracks.hits[i].track.images.coverart
-	// 		)
-	// 		shazamEl.setAttribute('href', response.tracks.hits[i].track.url)
-	// 		shazamEl.setAttribute('target', '_blank')
-	// 		resultsName.setAttribute('target', '_blank')
-	// 		resultImg.classList.add('img-fluid', 'd-inline-block', 'col-6')
-	// 		resultsName.classList.add('text-dark', 'd-inline-block', 'col-4')
-	// 		shazamEl.classList.add('card-text')
-	// 		shazamEl.classList.add('d-block')
-
-	// 		// appends create elements
-	// 		resultsEl.appendChild(resultsTab)
-	// 		resultsTab.appendChild(resultImg)
-	// 		resultsTab.appendChild(resultsName)
-
-	// 		resultsName.appendChild(shazamEl)
-
-	// 		// add responses to array
-
-	// 		searchByKeyword(resultsName, response.tracks.hits[i].track.share.subject)
-	// 	}
-	// }
-
-	// calls youtube api and sets anchor elements
-	// function searchByKeyword(name) {
-	// 	const options = {
-	// 		method: 'GET',
-	// 		headers: {
-	// 			'X-RapidAPI-Key': '7e218f9294mshed130077e09c28bp11d981jsnb67b9fe814b1',
-	// 			'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-	// 		},
-	// 	}
-
-	// 	fetch(
-	// 		'https://youtube-v31.p.rapidapi.com/search?q=' +
-	// 			searchEl.value +
-	// 			'&part=id%2Csnippet&type=video&maxResults=1',
-	// 		options
-	// 	)
-	// 		.then((response) => response.json())
-	// 		.then((response) => {
-	// 			console.log(response)
-	// 			let youtubeEl = document.createElement('a')
-	// 			youtubeEl.innerHTML = 'Listen on Youtube'
-	// 			youtubeEl.classList.add('youtube-link', 'd-block')
-	// 			youtubeEl.setAttribute(
-	// 				'href',
-	// 				'https://www.youtube.com/watch?v=' + response.items[0].id.videoId
-	// 			)
-	// 			youtubeEl.setAttribute('target', '_blank')
-	// 			name.appendChild(youtubeEl)
-	// 		})
-	// 		.catch((err) => console.error(err))
-	// }
-
-	// function reset(node1, node2, node3, node4, node5) {
-	// 	resultsEl.removeChild(node1)
-	// 	resultsEl.removeChild(node2)
-	// 	resultsEl.removeChild(node3)
-	// 	resultsEl.removeChild(node4)
-	// 	resultsEl.removeChild(node5)
-	// }
 
 	return (
 		<>
@@ -145,7 +58,7 @@ export const App = () => {
 					style={{ width: '350px' }}
 				/>
 				<p className='text-light p-2 header-sub'>
-					If you know the words, we'll find the song for you!
+					If you know the words, we know the song!
 				</p>
 			</header>
 			<main>
@@ -160,7 +73,11 @@ export const App = () => {
 							value={search}
 							onChange={(e) => setSearch(e.target.value)}
 						/>
-						<button type='submit' onClick={newSearch}>Submit</button>
+						<button
+							type='submit'
+							onClick={newSearch}>
+							Submit
+						</button>
 					</form>
 				</div>
 
